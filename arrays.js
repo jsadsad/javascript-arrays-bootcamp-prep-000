3995ce5d2fecb16142df254c892a28d095313105
JCS <joshsad2@gmail.com>
2017-09-12 07:44:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,24 +1,23 @@
-
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, ele) {
-  return [ele, ...array];
-}
-
-function destructivelyAddElementToBeginningOfArray(array, ele) {
-  array.unshift(ele);
-  return array;
-}
-
-function addElementToEndOfArray(array, element) {
-  return [array, ...element];
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
-}
-
-funtion accessElementInArray(array, index) {
-  return array[index];
-}
+// function addElementToBeginningOfArray(array, ele) {
+//   return [ele, ...array];
+// }
+//
+// function destructivelyAddElementToBeginningOfArray(array, ele) {
+//   array.unshift(ele);
+//   return array;
+// }
+//
+// function addElementToEndOfArray(array, element) {
+//   return [array, ...element];
+// }
+//
+// function destructivelyAddElementToEndOfArray(array, element) {
+//   array.push(element);
+//   return array;
+// }
+//
+// funtion accessElementInArray(array, index) {
+//   return array[index];
+// }
