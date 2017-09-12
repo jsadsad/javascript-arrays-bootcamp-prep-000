a527a6940fa064d523ac2e21ad8c5a3498117adf
JCS <joshsad2@gmail.com>
2017-09-12 07:42:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, ele) {
+  return [ele, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, ele) {
+  array.unshift(ele);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [array, ...element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+funtion accessElementInArray(array, index) {
+  return array[index];
+}
