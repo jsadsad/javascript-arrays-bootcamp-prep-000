2880e2118c5965c838883c8c4a2bd8e8dd86df00
JCS <joshsad2@gmail.com>
2017-09-12 07:45:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array;
 }
 
-// funtion accessElementInArray(array, index) {
-//   return array[index];
-// }
+function accessElementInArray(array, index) {
+  return array[index];
+}
